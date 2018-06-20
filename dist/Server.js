daa8facf599288c4069ce85b147b0d8b8c5c26bd
Christopher <christo_campiglia@hotmail.com>
2018-06-20 18:28:43 -0300

    [ADD][UPDATE] Stop static server when be needed.

--- dist/Server.js
+++ dist/Server.js
@@ -2,15 +2,24 @@ const CMD = require ("node-cmd");
 
 const PORT = 7373;
 
+let Pid;
+
+const Stop = () => {
+
+	CMD.run (`kill ${Pid}`);
+}
 
 const Start = () => {
 
-	CMD.get (`http-server ./dist -p ${PORT}`, 
+	const Process = CMD.get (`http-server ./dist -p ${PORT}`, 
 		(SomeError, Data, StdErr) => console.log ((SomeError ? SomeError : Data)));
 
 	console.log (`Serving On: http://localhost:${PORT}/`);
+
+	Pid = Process.pid;
 }
 
 Start ();
 
-module.exports = PORT;
+module.exports.PORT = PORT;
+module.exports.Stop = Stop;
