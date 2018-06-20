a230c3c4707ad62c28d0b183bae84bb1881c2362
Christopher <christo_campiglia@hotmail.com>
2018-06-20 08:57:09 -0300

    [ADD] Production basic server.

---
+++ dist/Server.js
@@ -0,0 +1,12 @@
+const Shell = require ("shelljs");
+
+const PORT = 7373;
+
+
+Shell.exec (`http-server ./dist -p ${PORT}`);
+
+
+console.log (`Serving On: http://localhost:${PORT}/`);
+
+
+module.exports.Port = Port;
